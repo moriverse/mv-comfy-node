23b45ef4565d5191de7be8aab27ac183511bc8cd
moriverse <dev@moriverse.xyz>
2024-11-07 13:19:13 +0800

    add on queue update for fal

--- fal.py
+++ fal.py
@@ -3,13 +3,16 @@ import typing as t
 
 
 def generate(prompt: str) -> t.Optional[str]:
+    def on_queue_update(update):
+        pass
+
     result = fal_client.subscribe(
         "fal-ai/flux-pro/v1.1-ultra",
         arguments={
             "prompt": prompt,
-            "raw": True,
             "aspect_ratio": "3:4",
         },
+        on_queue_update=on_queue_update,
     )
 
     images = result.get("images")
